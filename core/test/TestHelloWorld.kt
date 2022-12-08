5a545d5017c10748931f68e78ac725e9da909dfa
exertionRiver <exertionriver@gmail.com>
2022-12-07 22:26:35 -0700

    template for 1.11.0

---
+++ core/test/TestHelloWorld.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test
+
+class TestHelloWorld {
+
+    @Test
+    fun testHelloWorld() {
+        println("hello world!")
+    }
+
+}
---
+++ desktop/src/river/exertion/lkt/DesktopLauncher.kt
@@ -0,0 +1,14 @@
+package river.exertion.lkt
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = Lwjgl3ApplicationConfiguration()
+        config.setForegroundFPS(60)
+        config.setTitle("libKTXTemplate")
+        Lwjgl3Application(Game(), config)
+    }
+}
